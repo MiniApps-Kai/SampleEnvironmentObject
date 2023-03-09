ff00ffd92f927275df7a4cf08d5744014daed4d5
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-03-10 01:23:52 +0900

    Initial Commit

---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by 渡邊魁優 on 2023/03/10.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
